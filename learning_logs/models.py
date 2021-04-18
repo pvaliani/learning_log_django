07c474751b21e73b58c1019a27534e59732d3f11
Pedram Valiani <pedram@Pedrams-MacBook-Pro.local>
2021-04-18 20:45:32 +0100

    config to settings - added app

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Topic(models.Model):
     """A topic the user is learning about."""
-    text = models.CharField(max_length==200)
+    text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
