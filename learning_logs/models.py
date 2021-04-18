aa6b134b95c04d6e70b882628d9018945a63754d
Pedram Valiani <pedram@Pedrams-MacBook-Pro.local>
2021-04-18 21:12:16 +0100

    adds comments for entry model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -10,17 +10,24 @@ class Topic(models.Model):
     def __str__(self):
         """Return a string representation of the model."""
         return self.text
-    
+
+
+
 
 class Entry(models.Model):
     """Something specific learning about a topic"""
+    """When a topic is deleted - delete all related entries"""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
+    """Meta class allows us to set a special attribute telling
+    Django to use Entries when it needs to refer to more than one entry"""
     class Meta:
         verbose_name_plural = 'entries'
 
     def __str__(self):
         """Return a string representation of the model"""
+        """Django returns the first 50 characters of text so
+        that we are not always displaying the full entry"""
         return f"{self.text[:50]}..."
